72d79fba375cd69ce1064b864d1b538ae8d0973f
Grant Guo <gguo@olm-gguo.oicr.on.ca>
2018-02-05 10:57:14 -0500

    call ClasspathURLConnection

--- common/src/main/scala/grant/analytics/common/url/ClasspathURLStreamHandler.scala
+++ common/src/main/scala/grant/analytics/common/url/ClasspathURLStreamHandler.scala
@@ -17,21 +17,3 @@ class ClasspathURLStreamHandler extends URLStreamHandler{
     new ClasspathURLConnection(u)
   }
 }
-
-class ClasspathURLConnection(url:URL) extends URLConnection(url) {
-  override def connect(): Unit = {
-    getClass.getResource(url.getPath).openConnection()
-  }
-
-  /**
-    * by override this method, i could program like the following codes:
-    *
-    * new URL(...).openStream()
-    *
-    * @return
-    */
-  override def getInputStream: InputStream = {
-    getClass.getResourceAsStream(url.getPath)
-  }
-
-}
